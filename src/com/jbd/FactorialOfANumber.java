d8cd6668bf7b98c3b992d9e9851802c6a76e0461
Digvijay Jatkar <jatkardigvijay@gmail.com>
2023-07-20 22:38:22 +0530

    daily 2 programs

---
+++ src/com/jbd/FactorialOfANumber.java
@@ -0,0 +1,23 @@
+package com.jbd;
+
+import java.util.Scanner;
+
+public class FactorialOfANumber {
+
+	public static void main(String[] args) {
+
+		int number, factorial = 1, temp;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		number = input.nextInt();
+		input.close();
+		temp = number;
+		while (number > 0) {
+
+			factorial = factorial * number;
+			number--;
+		}
+
+		System.out.println("Factorial of " + temp + " is : " + factorial);
+	}
+}
---
+++ src/com/jbd/Vowel.java
@@ -0,0 +1,20 @@
+package com.jbd;
+
+import java.util.Scanner;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+
+		char ch;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a character : ");
+		ch = input.next().charAt(0);
+		input.close();
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println("Entered character is a vowel");
+		} else {
+			System.out.println("Entered character is not a vowel");
+		}
+	}
+}
